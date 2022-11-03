e2262aabe06501c3fc2d1a0c344068548871013a
Matt Reynolds <matt.reynolds@smartvisionlights.com>
2022-11-03 17:12:23 -0400

    skeleton complete, need to trigger cam

--- ArduinoCode/test.ino
+++ ArduinoCode/test.ino
@@ -1,15 +1,16 @@
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600); // Set Baud Rate for Serial Communication
   Serial.println("Serial Started: Waiting for input");
-}
+}//end setup()
 
-/**FUNCTION PROTOTYPES**/
-int contMode();
-int overDrive();
-int multiDrive();
-int dubOverDrive();
+/**FUNCTION PROTOTYPES - Measurement Protocols**/
+int contMode();     // Continuous Mode
+int overDrive();    // OverDrive Mode
+int multiDrive();   // MultiDrive Mode
+int dubOverDrive(); // Double OverDrive Mode
 
 char input = 0;
+String outString = "";
 
 void loop() {
   if (Serial.available() > 0){
@@ -17,40 +18,47 @@ void loop() {
     if(input != '\n'){
         switch (input)
         {
-        case 'C':
+        case 'C': // Continuous Mode
             Serial.println("Continuous");
+            outString = contMode();             // Calls Continuous Measurment Protocol, Returns all Peak Current Measurments
             break;
-        case 'O':
+        case 'O': // OverDrive Mode
             Serial.println("OverDrive");
+            outString = overDrive();            // Calls OverDrive Measurment Protocol, Returns all Peak Current Measurments
             break;
-        case 'M':
+        case 'M': // MultiDrive Mode
             Serial.println("MultiDrive");
+            outString = multiDrive();           // Calls MultiDrive Measurment Protocol, Returns all Peak Current Measurments
             break;
-        case 'D':
+        case 'D': // Double OverDrive Mode
             Serial.println("Double OverDrive");
+            outString = dubOverDrive();         // Calls Double Overdrive Measurment Protocol, Returns all Peak Current Measurments
             break;
         default:
             Serial.println("Invalid Entry");
+            outString = "Invalid Entry: 0";
             break;
-        }
-    }
-  }
-}
+        }//end switch
+
+        Serial.println(outString); // Send Data to PC
+    }//end if
+  }//end if
+}//end loop()
 
 
-/**USER DEFINED FUNCTIONS**/
+/**USER DEFINED FUNCTIONS - Measurement Protocols**/
 int contMode(){
-
-}
+    return 10;
+}//end contMode()
 
 int overDrive(){
-
-}
+    return 11;
+}//end overDrive
 
 int multiDrive(){
-
-}
+    return 12;
+}//end multiDrive()
 
 int dubOverDrive(){
-
-}
+    return 13;
+}//end dubOverDrive()
