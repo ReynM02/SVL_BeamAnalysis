cc3409b725f22e7c051d6bc25dae2d1be8a88e27
Matt <100549488+ReynM02@users.noreply.github.com>
2022-12-19 17:00:22 -0500

    fixed file pathing

--- src/measureLight.py
+++ src/measureLight.py
@@ -175,7 +175,7 @@ def CaptureExt(cam, mode, exp, config):
 #End CaptureExt() 
 
 def loadConfig(light_string):
-    filePath = "C:/Users/SVL226/Documents/GitHub/SVL_BeamAnalysis/configs/"+light_string+".json"
+    filePath = documentPath + "/configs/" + light_string + ".json"
     try:
         with open(filePath, 'r') as file:
             print("found config")
@@ -445,9 +445,11 @@ def measure(light_string, cam):
 
 #### DEBUG MODE ####
 #with Vimba.get_instance() as vimba:
-    cams = vimba.get_all_cameras()
-    cams[0].set_access_mode(AccessMode.Full)
-    with cams[0] as cam:
-        cam.load_settings("C:/Users/SVL226/Documents/GitHub/SVL_BeamAnalysis/EOLTestSettings.xml", PersistType.NoLUT)
-        connect()
-        measure("JWL150-MD-WHI", cam)
+#    cams = vimba.get_all_cameras()
+#    cams[0].set_access_mode(AccessMode.Full)
+#    with cams[0] as cam:
+#        documentPath = "C:/Users/SVL226/Documents/EOLTester"
+#        path = documentPath + "/src/EOLTestSettings.xml"
+#        cam.load_settings(path, PersistType.NoLUT)
+#        connect()
+#        measure("JWL150-MD-WHI", cam)
