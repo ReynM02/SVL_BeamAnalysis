c0e0b6a10ab96c17b0f39a8cc3ac34f3ef2bb5c4
Matt Reynolds <matt.reynolds@smartvisionlights.com>
2022-12-22 16:33:08 -0500

    setup DO protocol with 3.5% DC

--- src/ArduinoCode/SLAElectronicsTests.ino
+++ src/ArduinoCode/SLAElectronicsTests.ino
@@ -131,9 +131,33 @@ void NPNStrobe(){
     Wire.begin(0x70);
     for(int x = 0; x < 200; x++){
             digitalWrite(NPNPin, HIGH);
-            delay(2);
+            delay(1);
             digitalWrite(NPNPin, LOW);
-            delay(3);
+            delay(10);
+        }
+    Wire.end();
+}
+
+void NPNStrobeDO(){
+    Wire.begin(0x70);
+    for(int x = 0; x < 200; x++){
+            digitalWrite(NPNPin, HIGH);
+            delay(.35);
+            digitalWrite(NPNPin, LOW);
+            delay(10);
+        }
+    Wire.end();
+}
+
+void PNPStrobeDO(int level){
+    dac.setDACOutVoltage(level, PNP); // light on PNP
+    delay(100);
+    Wire.begin(0x70);
+    for(int x = 0; x < 200; x++){
+            digitalWrite(dPNP, HIGH);
+            delay(.35);
+            digitalWrite(dPNP, LOW);
+            delay(10);
         }
     Wire.end();
 }
@@ -144,9 +168,9 @@ void PNPStrobe(int level){
     Wire.begin(0x70);
     for(int x = 0; x < 200; x++){
             digitalWrite(dPNP, HIGH);
-            delay(2);
+            delay(1);
             digitalWrite(dPNP, LOW);
-            delay(3);
+            delay(10);
         }
     Wire.end();
 }
@@ -241,15 +265,18 @@ String multiDrive(){
 
 String dubOverDrive(){
     String currentStr = "";
-    dac.setDACOutVoltage(0, analog);
-    PNPStrobe(5000);
+    dac.setDACOutVoltage(5000, analog); // 10v Analog
+    PNPStrobeDO(5000);
     currentStr += peakCurrent;
     currentStr += ",";
     delay(100);
-    NPNStrobe();
+    NPNStrobeDO();
     currentStr += peakCurrent;
     currentStr += ",";
-    PNPStrobe(3000);
+    delay(100);
+    dac.setDACOutVoltage(3000, analog); // 5v Analog
+    delay(500);
+    PNPStrobeDO(3000);
     currentStr += peakCurrent;
     return currentStr;
 }//end dubOverDrive()
@@ -259,7 +286,7 @@ bool triggerCam(char mode){
     Serial.write("S");
     delay(200);
     digitalWrite(camPin, HIGH);
-    delay(100);
+    delay(expTime);
     digitalWrite(camPin, LOW);
     serialHandshake();
     //Foreground Image
